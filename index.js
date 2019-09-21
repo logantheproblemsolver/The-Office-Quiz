f9e9593b144b17c5c4cb75ff08abb07cac87e1ef
Logan Ellis <mrloganellis@gmail.com>
2019-09-20 21:18:35 -0400

    Added JS File

---
+++ index.js
@@ -0,0 +1,22 @@
+function scoreChecker() {
+
+}
+
+function answerValidator() {
+    
+}
+
+
+/*
+function transitionBetweenQuestions() {
+    this is a possible one, still not sure yet
+}
+*/
+
+
+function initialize() {
+    scoreChecker();
+    answerValidator();
+}
+
+$(initialize);
