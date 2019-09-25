24ff7a95f0d125f091065ae7a7b6bab331eb5414
Logan Ellis <mrloganellis@gmail.com>
2019-09-24 22:57:05 -0400

    Everything is finished, and I just need to add a background!

--- index.js
+++ index.js
@@ -2,48 +2,89 @@
 
 let QUESTIONS = [
     {
-        'question': 'What is Dwights last name?',
-        'choice1': 'Halpert',
-        'choice2': 'Schrute',
-        'choice3': 'Bernard',
-        'choice4': 'Scott',
-        'correctAnswer': 'Schrute'
+        'question': 'Who was Ryan\'s son named afer?',
+        'choice1': 'Drake the rapper',
+        'choice2': 'Drake the bird',
+        'choice3': 'His dad',
+        'choice4': 'A combination between Drew and Blake',
+        'correctAnswer': 'A combination between Drew and Blake'
     },
 
     {
-        'question': 'What is Jim\'s last name?',
-        'choice1': 'Scott',
-        'choice2': 'Beezly',
-        'choice3': 'Halpert',
-        'choice4': 'Malone',
-        'correctAnswer': 'Halpert'
+        'question': 'What did Michael win an award for in Floggerton?',
+        'choice1': 'Fastest typer',
+        'choice2': 'Signing papers on his condo',
+        'choice3': 'Organizing a 5k',
+        'choice4': 'Best Regional Manager',
+        'correctAnswer': 'Signing papers on his condo'
     },
 
     {
-        'question': 'Who was Pam first engaged to?',
-        'choice1': 'Jim',
-        'choice2': 'Kevin',
-        'choice3': 'Roy',
-        'choice4': 'Dwight',
-        'correctAnswer': 'Roy'
+        'question': 'Which Harry Potter book does Dwight take with him in the game dessert island?',
+        'choice1': 'Prisoner of Askaban',
+        'choice2': 'Goblet of Fire',
+        'choice3': 'Order of the Pheonix',
+        'choice4': 'Sorcerer\'s Stone',
+        'correctAnswer': 'Prisoner of Askaban'
     },
 
     {
-        'question': 'Who did Michael Scott hit with a car?',
-        'choice1': 'Pam',
-        'choice2': 'Jan',
+        'question': 'What is Andy Bernard\'s extension?',
+        'choice1': '156',
+        'choice2': '134',
+        'choice3': '911',
+        'choice4': '178',
+        'correctAnswer': '134'
+    },
+
+    {
+        'question': 'Who does Micahel fire on Halloween?',
+        'choice1': 'Kevin',
+        'choice2': 'Andy',
+        'choice3': 'Angela',
+        'choice4': 'Devon',
+        'correctAnswer': 'Devon'
+    },
+    
+    {   'question': 'Why did Michael wait until Halloween to fire someone?',
+        'choice1': 'It\'s really scary stuff',
+        'choice2': 'It was time to finally fire him',
+        'choice3': 'I felt like it',
+        'choice4': 'Jan made me do it',
+    },
+    
+    {   'question': 'How much money does Nelly give Darrel to buy tacos at the animal fundraiser?',
+        'choice1': '$30',
+        'choice2': '$10',
+        'choice3': '$40',
+        'choice4': '$5',
+        'correctAnswer': '$30',
+    },
+   
+    {   'question': 'What does David Wallace auction off in the Crime Aid Auction?',
+        'choice1': 'A pay raise',
+        'choice2': 'The monthly bonus',
+        'choice3': 'A weekend at his house in Martha\'s Vineyard',
+        'choice4': 'A weekend at his house in New York',
+        'correctAnswer': 'A weekend at his house in Martha\'s Vineyard',
+
+    },
+    
+    {   'question': 'What are the three closets designated for during the PDA episode?',
+        'choice1': 'One for each cleaning task',
+        'choice2': 'One for each base, no homers',
+        'choice3': 'One for each girl in The Office, except Meredith',
+        'choice4': 'They act as regular closets, nothing weird',
+        'correctAnswer': 'One for each base, no homers',
+    },
+    
+    {   'question': 'What does Ryan think Creed smells like?',
+        'choice1': 'Death',
+        'choice2': 'Old Man',
         'choice3': 'Meredith',
-        'choice4': 'Phyllis',
-        'correctAnswer': 'Meredith'
-    },
+        'choice4': 'Petunias',
+        'correctAnswer': 'Old Man',
 
-    {
-        'question': 'What did Andy call Jim?',
-        'choice1': 'Big Tuna',
-        'choice2': 'Fish',
-        'choice3': 'Mentor',
-        'choice4': 'Big Fish',
-        'correctAnswer': 'Big Tuna'
     },
 ]
 
@@ -66,7 +107,8 @@ function generateQuizQuestions() {
                 <input type="radio" id='${QUESTIONS[numberQuestion].choice4}'  name= 'question' value="${QUESTIONS[numberQuestion].choice4}"><label for='${QUESTIONS[numberQuestion].choice4}'>${QUESTIONS[numberQuestion].choice4}</label>
             </div>
             <div class='answerButton'>
-                <button type='submit' id='theAnswerButton'>Answer</button>
+                <button type='submit' id='theAnswerButton' class="button">Answer</button>
+            </div>
         </form>
     </section>`;
     return quizQuestion;
@@ -83,20 +125,20 @@ function startQuiz() {
 }
 
 function showAnswer() {
-    $('.question').on('click', '#theAnswerButton', function (a) {
-        a.preventDefault();
-        $('.question').find('.answerButton').hide();
-        if ($(!'input[name="question"]').is('.checked')) {
+    $('.question').on('click', '#theAnswerButton', function (q) {
+        q.preventDefault();
+        if (!$('input[name="question"]').is(':checked')) {
             alert('Please select an option')
         } else {
+            $('.question').find('.answerButton').hide();
             $('.answer').show();
             let radioValue = $('input[name="question"]:checked').val();
             console.log(radioValue);
             if (radioValue === QUESTIONS[numberQuestion].correctAnswer) {
-                testScore += 20;
-                $('.answer').html(`<p>You're correct!</p><button type='button' id='nextQuestion'>Next Question</button>`);
+                testScore += 10;
+                $('.answer').html(`<img class="correct" src="Andy-NailedIt.gif" alt="You're correct!"/><p>You're correct!</p><button type='button' id='nextQuestion' class="button">Next Question</button>`);
             } else {
-                $('.answer').html(`<p> Sorry, the correct answer was ${QUESTIONS[numberQuestion].correctAnswer}.</p><button type='button' id='nextQuestion'>Next Question</button>`);
+                $('.answer').html(`<img class="false" src="Dwight-False.jpg" height="270" width=="170" alt="Incorrect answser"/img><p> Sorry, the correct answer was ${QUESTIONS[numberQuestion].correctAnswer}.</p><button type='button' id='nextQuestion' class="button">Next Question</button>`);
             }
             numberQuestion++;
         }
@@ -106,7 +148,7 @@ function showAnswer() {
 function nextQuestion() {
     $('.answer').on('click', '#nextQuestion', function (n) {
         n.preventDefault();
-        if (numberQuestion >= 5) {
+        if (numberQuestion >= 10) {
             completeQuiz();
         } else {
             $('.answer').val = '';
@@ -119,7 +161,6 @@ function nextQuestion() {
 }
 
 function updateScore() {
-    
     $('.testScore').html(`<p>Current score is ${testScore}/100</p>`)
 }
 
@@ -150,7 +191,7 @@ function completeQuiz() {
             <h1>The Office Trivia</h1>
         <section="quizStarter">
             <p> Do you think you're the most knowledgeable on The Office? Have you seen all the shows 12 times like a true fan? Come and test your knowledge! </p>
-            <button id="startButton" type="button">Start</button>
+            <button id="startButton" type="button" class="button">Start</button>
         </section>
         <section class="question"></section>
         <section class="answer"></section>
